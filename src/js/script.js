17f181855161137492ed03a7a5a4f44a87a40f48
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-06-09 00:48:32 +0200

    Fixed subTotalPrice

--- src/js/script.js
+++ src/js/script.js
@@ -38,7 +38,7 @@
       toggleTrigger: '.cart__summary',
       totalNumber: `.cart__total-number`,
       totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
-      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+      subTotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
       deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
       form: '.cart__order',
       formSubmit: '.cart__order [type="submit"]',
@@ -439,7 +439,7 @@
       
       
       thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
-      //thisCart.dom.subTotalPrice.innerHTML = thisCart.subTotalPrice;    
+      thisCart.dom.subTotalPrice.innerHTML = thisCart.subTotalPrice;    
             
     }
   }
