1ce85b70fcf879577ce7fa52f58e84999b6a53b8
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-06-10 23:39:25 +0200

    Add sendOrder()

--- src/js/script.js
+++ src/js/script.js
@@ -394,8 +394,7 @@
     initActions(){
       const thisCart = this;
 
-      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
-        event.preventDefault();
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
       thisCart.dom.productList.addEventListener('updated', function(){
@@ -451,6 +450,42 @@
       thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
       thisCart.dom.subTotalPrice.innerHTML = thisCart.subTotalPrice;   
       thisCart.dom.deliveryFee.innerHTML = thisCart.deliveryFee;            
+    }
+
+    sendOrder() {
+
+      const thisCart = this;
+  
+      const url = settings.db.url + '/' + settings.db.orders;
+
+      const payload = {
+        address: thisCart.dom.address,
+        phone: thisCart.dom.phone,
+        totalPrice: thisCart.totalPrice,
+        subtotalPrice: thisCart.subtotalPrice,
+        totalNumber: thisCart.totalNumber,
+        deliveryFee: thisCart.deliveryFee,
+        products: [],
+      };
+  
+      for (let prod of thisCart.products) {
+        payload.products.push(prod.getData());
+      }
+  
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+  
+      fetch(url, options)
+        .then(function(response){
+          return response.json();
+        }).then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+        });
     }
 
     remove(cartProduct){
@@ -530,6 +565,20 @@
 
       thisCartProduct.dom.wrapper.dispatchEvent(event);
       console.log('remove');
+    }
+
+    getData() {
+
+      const thisCartProduct = this;
+  
+      const products = {
+        id: thisCartProduct.id,
+        amount: thisCartProduct.amount,
+        price: thisCartProduct.price,
+        priceSingle: thisCartProduct.priceSingle,
+        params: thisCartProduct.params,
+      };
+      return products;
     }
   }
   const app = {
