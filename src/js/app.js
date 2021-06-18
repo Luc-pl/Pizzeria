25602b63e22f46b3b1b7297bcd443136e33f99b2
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-06-18 08:36:39 +0200

    Add initPages

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,32 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links).children;
+
+    thisApp.activatePage(thisApp.pages[0].id);
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
 
@@ -48,7 +72,10 @@ const app = {
     thisApp.productList.addEventListener('add-to-cart', function(event){
       app.cart.add(event.detail.product);
     });
+
+    thisApp.initPages();
   },
+  
 };
 
 //app.initMenu();
