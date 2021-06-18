0bcc9e8701382204b2795f588d0e1e031c094285
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-06-18 12:23:44 +0200

    Add class Booking code

---
+++ src/js/components/Booking.js
@@ -0,0 +1,27 @@
+import { templates, select } from "../settings.js";
+import AmountWidget from './AmountWidget.js'
+
+class Booking {
+  constructor(element){
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element){
+    const thisBooking = this;
+
+    const generatedHTML = templates.bookingWidget();
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+  }
+
+
+
+
+}
+
+export default Booking;
